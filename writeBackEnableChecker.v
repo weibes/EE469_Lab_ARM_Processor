b6c1604207b7db63bdab67f74e1b9e23bfaee0f0
Brendan Weibel <weibeb@uw.edu>
2020-02-19 16:37:15 -0800

    implemented writeBackEnable

--- writeBackEnableChecker.v
+++ writeBackEnableChecker.v
@@ -1,11 +1,10 @@
 module writeBackEnableChecker(notBran);
 	input wire notBranch, condMet, writeBackBit;
-	input wire [4:0] opcode;
 	
-	output wire writeBackEnable
+	output wire writeBackEnable;
 
 	always @* begin
-		writeBackEnable = ((opcode != 5'b10001) && condMet)
+		writeBackEnable = (notBranch && condMet)
 	end
 	
 
