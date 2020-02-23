f1a83b3704d3e4bb34ff7649aaa2770af4bfddb6
totesmgoats <58083590+totesmgoats@users.noreply.github.com>
2020-02-22 17:48:54 -0800

    fixed writebackEnable

--- DataMemoryRegister.v
+++ DataMemoryRegister.v
@@ -1,12 +1,13 @@
-module DataMemoryRegister(dataMemOut_DMR, rd_DMR, linkBit, dataMemOut_DMR_OUT,  rd_DMR_OUT, linkBit_DMR_OUT, reset, clk);
+module DataMemoryRegister(dataMemOut_DMR, rd_DMR, linkBit,writebackEnable, dataMemOut_DMR_OUT,  rd_DMR_OUT, linkBit_DMR_OUT, writebackEnable_DMR_OUT, reset, clk);
 
-input wire linkBit, reset, clk;	
-input wire dataMemOut_DMR;
-input wire rd_DMR;
+input wire linkBit, writebackEnable, reset, clk;	
+input wire [31:0] dataMemOut_DMR;
+input wire [3:0] rd_DMR
 
-output reg dataMemOut_DMR_OUT;
-output reg rd_DMR_OUT;
-output reg linkBit_DMR_OUT;
+
+output reg [31:0] dataMemOut_DMR_OUT;
+output reg [3:0] rd_DMR_OUT;
+output reg linkBit_DMR_OUT, writebackEnable_DMR_OUT;
 
 	 always @(posedge clk) begin
 		 if (reset) begin
@@ -14,12 +15,14 @@ output reg linkBit_DMR_OUT;
 		dataMemOut_DMR_OUT <= 0;
 		rd_DMR_OUT <= 0;
 		linkBit_DMR_OUT <= 0;
+		writebackEnable_DMR_OUT <= 0;
 		
 		 end
 		 else begin
 		dataMemOut_DMR_OUT <= dataMemOut_DMR;
 		rd_DMR_OUT <= rd_DMR;
 		linkBit_DMR_OUT <= linkBit;
+		writebackEnable_DMR_OUT <= writebackEnable;
 		end			
 end
 endmodule
